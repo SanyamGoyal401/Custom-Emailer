a70e9166f82825d178651e93e396978c548a2f52
Sanyam Goyal <sanyamgoyal2859@gmail.com>
2023-08-19 17:57:09 +0530

    fixed environment variables

--- index.js
+++ index.js
@@ -27,33 +27,39 @@ app.post("/mail", (req, res) => {
   let transporter = nodemailer.createTransport({
     service: "gmail",
     auth: {
-      user: process.env.USER,
-      pass: process.env.PASS,
+      user: process.env.user,
+      pass: process.env.pass,
     },
     tls: {
       rejectUnauthorized: false,
     },
   });
-  res.status(200).json({message: 'mail queued for delivery'});
-  setTimeout(()=>{
+
+  try{
+
     transporter.sendMail(mailOptions, (error, info) => {
-        if (error) {
-          console.log(error);
-        } else {
-            console.log(info);
-        }
-      });
-  }, delay * 1000);
-  
+      if (error) {
+        console.log(error);
+      } else {
+        console.log(info);
+      }
+    }, delay * 1000);
+    
+    res.status(200).json({ message: 'mail queued for delivery' });
+  }
+  catch(e){
+    res.status(400).json({message: 'some error occured'});
+  }
+
 });
 
 
 
 app.listen(PORT, (err) => {
-  if(err){
+  if (err) {
     console.log(err);
   }
-  else{
-    console.log('port is running');
+  else {
+    console.log('port is running at ', PORT);
   }
 });
